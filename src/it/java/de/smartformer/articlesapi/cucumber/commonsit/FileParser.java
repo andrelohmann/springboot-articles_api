623829f9be896419a03fed2780ea9a52a0cad708
jehadhnasser@gmail.com <jehadhnasser@gmail.com>
2020-12-02 19:27:43 +0100

    added fileWatcher and fileParser

---
+++ src/it/java/de/smartformer/articlesapi/cucumber/commonsit/FileParser.java
@@ -0,0 +1,31 @@
+package de.smartformer.articlesapi.cucumber.commonsit;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.regex.Pattern;
+
+public class FileParser {
+
+    public static String readFromPointer(String fileName, long pointer) {
+        try (RandomAccessFile file = new RandomAccessFile(fileName, "r")) {
+            // Move the cursor in the file
+            file.seek(pointer);
+            byte[] arr = new byte[(int) file.length() - (int) pointer];
+            file.readFully(arr);
+            return new String(arr);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return ex.getMessage().toString();
+        }
+    }
+
+    public static boolean isStringMatch(String allText, String targetText) {
+
+        allText = allText.replaceAll("\\[|\\]", "");
+        targetText = targetText.replaceAll("\\[|\\]", "");
+        Pattern p = Pattern.compile(targetText, Pattern.MULTILINE);
+
+        return p.matcher(allText).find();
+    }
+}
